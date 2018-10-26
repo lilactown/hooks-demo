3e76fffa1b26c3afd0ac0ace71875bc51f7dbae4
Will Acton <will@willacton.com>
2018-10-26 10:10:32 -0700

    moar comments

--- src/hooks_demo.cljs
+++ src/hooks_demo.cljs
@@ -22,17 +22,20 @@
 ;;
 
 (defn use-atom
+  ;; if no deps are passed in, we assume we only want to run
+  ;; subscrib/unsubscribe on mount/unmount
   ([a] (use-atom a []))
   ([a deps]
+   ;; create a react/useState hook to track and trigger renders
    (let [[v u] (react/useState @a)]
-     ;; useEffect creates the subscription to the atom
+     ;; react/useEffect hook to create and track the subscription to the iref
      (react/useEffect
       (fn []
         (println "adding watch")
         (add-watch a :use-atom
                    ;; update the react state on each change
                    (fn [_ _ _ v'] (u v')))
-        ;; return a function to tell react how to unsubscribe
+        ;; return a function to tell react hook how to unsubscribe
         #(do
            (println "removing watch")
            (remove-watch a :use-atom)))
