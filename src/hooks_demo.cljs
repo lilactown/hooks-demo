cb25c49b2f06ad10fab5c28a6a5d440d4cd446c1
Will Acton <will@willacton.com>
2018-10-26 18:21:30 -0700

    separate hooks and fancy syntax

--- src/hooks_demo.cljs
+++ src/hooks_demo.cljs
@@ -1,6 +1,6 @@
 (ns hooks-demo
-  (:require ["react" :as react]
-            ["react-dom" :as react-dom]
+  (:require ["react-dom" :as react-dom]
+            [hooks-demo.hooks :as hooks]
             [hx.react :as hx :include-macros true]))
 
 ;;
@@ -8,53 +8,35 @@
 ;;
 
 (hx/defnc UseState [_]
-  (let [[count set-count] (react/useState 0)]
+  (let [[count set-count] (hooks/!>state 0)]
     (hx/c [:div
            [:strong "UseState"]
            " "
            [:button {:on-click #(set-count (inc count))}
             count]])))
 
-;;
-;; Example of a custom use-atom hook
-;;
-
-(defn use-atom
-  ;; if no deps are passed in, we assume we only want to run
-  ;; subscrib/unsubscribe on mount/unmount
-  ([a] (use-atom a []))
-  ([a deps]
-   ;; create a react/useState hook to track and trigger renders
-   (let [[v u] (react/useState @a)]
-     ;; react/useEffect hook to create and track the subscription to the iref
-     (react/useEffect
-      (fn []
-        (println "adding watch")
-        (add-watch a :use-atom
-                   ;; update the react state on each change
-                   (fn [_ _ _ v'] (u v')))
-        ;; return a function to tell react hook how to unsubscribe
-        #(do
-           (println "removing watch")
-           (remove-watch a :use-atom)))
-      ;; pass in deps vector as an array
-      (clj->js deps))
-     ;; return value of useState on each run
-     v)))
-
 (defonce my-state (atom 0))
 
-(hx/defnc UseAtom [_]
-  (let [count (use-atom my-state)]
+(hx/defnc UseDeref [_]
+  (let [count (hooks/!>deref my-state)]
     (hx/c [:div
-           [:strong "UseAtom"]
+           [:strong "UseDeref"]
            " "
            [:button {:on-click #(swap! my-state inc)}
             count]])))
 
+(hx/defnc UseAtom [_]
+  (let [count (hooks/!>atom 0)]
+    (hx/c [:div
+           [:strong "UseAtom"]
+           " "
+           [:button {:on-click #(swap! count inc)}
+            @count]])))
+
 (hx/defnc App [_]
   (hx/c [:div
          [UseState]
+         [UseDeref]
          [UseAtom]]))
 
 (react-dom/render (hx/c [App]) (js/document.getElementById "app"))
---
+++ src/hooks_demo/hooks.cljs
@@ -0,0 +1,53 @@
+(ns hooks-demo.hooks
+  (:require ["react" :as react]))
+
+(def !>state react/useState)
+
+(def !>effect react/useEffect)
+
+;;
+;; Example of a custom with-atom hook
+;;
+
+(defn !>deref
+  ;; if no deps are passed in, we assume we only want to run
+  ;; subscrib/unsubscribe on mount/unmount
+  ([a] (!>deref a []))
+  ([a deps]
+   ;; create a react/useState hook to track and trigger renders
+   (let [[v u] (!>state @a)]
+     ;; react/useEffect hook to create and track the subscription to the iref
+     (!>effect
+      (fn []
+        (println "adding watch")
+        (add-watch a :use-atom
+                   ;; update the react state on each change
+                   (fn [_ _ _ v'] (u v')))
+        ;; return a function to tell react hook how to unsubscribe
+        #(do
+           (println "removing watch")
+           (remove-watch a :use-atom)))
+      ;; pass in deps vector as an array
+      (clj->js deps))
+     ;; return value of useState on each run
+     v)))
+
+;; credit Roman Liutikov (@roman01la)
+;; https://twitter.com/roman01la/status/1055905216166543361
+
+(deftype State [react-ref]
+  IDeref
+  (-deref [_]
+    (first react-ref))
+
+  IReset
+  (-reset! [_ v']
+    ((second react-ref) v')
+    v')
+
+  ISwap
+  (-swap! [o f]
+    (-reset! o (f (-deref o)))))
+
+(defn !>atom [initial]
+  (State. (!>state initial)))
