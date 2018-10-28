249deb3a2ffcf39b3250c1485eb7f02aaf32357d
Will Acton <will@willacton.com>
2018-10-28 12:41:48 -0700

    use reducer hook

--- src/hooks_demo.cljs
+++ src/hooks_demo.cljs
@@ -1,33 +1,39 @@
 (ns hooks-demo
   (:require ["react-dom" :as react-dom]
             ["react" :as react]
-            [hooks-demo.hooks :as hooks :refer [<-state <-context]]
+            [hooks-demo.hooks :as hooks :refer [<-reducer <-context]]
             [hx.react :as hx :include-macros true]))
 
 (defonce state-context (react/createContext))
 
 (def Provider (.-Provider state-context))
 
+(defn reducer [state [type value]]
+  (case type
+    :demo/update-name (assoc state :name value)
+    :demo/inc-count (update state :count + value)
+    :demo/dec-count (update state :count - value)))
+
 (hx/defnc Display [_]
-  (let [{:keys [name count]} @(<-context state-context)]
+  (let [[{:keys [name count]} _] (<-context state-context)]
     (hx/c [:div name ": " count])))
 
 (hx/defnc Input [_]
-  (let [!state (<-context state-context)]
+  (let [[{:keys [name]} dispatch!] (<-context state-context)]
     (hx/c [:div
-           [:input {:type "text" :value (:name @!state)
+           [:input {:type "text" :value name
                     :on-change
-                    #(swap! !state assoc :name (.. % -target -value))}]])))
+                    #(dispatch! [:demo/update-name (.. % -target -value)])}]])))
 
 (hx/defnc CountButtons [_]
-  (let [!state (<-context state-context)]
+  (let [[_ dispatch!] (<-context state-context)]
     (hx/c [:div
-           [:button {:on-click #(swap! !state update :count inc)} "inc"]
-           [:button {:on-click #(swap! !state update :count dec)} "dec"]])))
+           [:button {:on-click #(dispatch! [:demo/inc-count 1])} "inc"]
+           [:button {:on-click #(dispatch! [:demo/dec-count 1])} "dec"]])))
 
 (hx/defnc App [_]
-  (let [!state (<-state {:name "Will" :count 1})]
-    (hx/c [Provider {:value !state}
+  (let [state (<-reducer reducer {:name "Will" :count 1})]
+    (hx/c [Provider {:value state}
            [:div
             [Display]
             [Input]
